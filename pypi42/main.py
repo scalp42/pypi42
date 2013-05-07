56028f1747dfca6e0209358c203faa5dd21c4fd4
Anthony Scalisi <scalisi.a@gmail.com>
2013-05-06 22:06:32 -0700

    get path of site-packages

--- pypi42/main.py
+++ pypi42/main.py
@@ -1,8 +1,8 @@
 def pypi42():
     from fabric.main import main
+    from fabric import state
+    from distutils.sysconfig import get_python_lib
+
     print "HELLO WORLD 42"
+    print(get_python_lib())
     main()
-
-
-def toto():
-    print "TOTO"
