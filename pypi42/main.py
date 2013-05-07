831d6889e508755e1839096a79c92a423c3aa9b8
Anthony Scalisi <scalisi.a@gmail.com>
2013-05-06 22:13:46 -0700

    try to os.chdir to site-packages

--- pypi42/main.py
+++ pypi42/main.py
@@ -1,8 +1,14 @@
+import os
+
+
 def pypi42():
     from fabric.main import main
     from fabric import state
     from distutils.sysconfig import get_python_lib
 
     print "HELLO WORLD 42"
-    print(get_python_lib())
+    savedpath = os.getcwd()
+    fabpath = get_python_lib() + '/pypi42'
+    os.chdir(fabpath)
     main()
+    os.chdir(savedpath)
